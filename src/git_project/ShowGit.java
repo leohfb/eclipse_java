12bf6568b1f2af873e0646708332f398d581f06a
leohfb <77646829+leohfb@users.noreply.github.com>
2021-01-18 20:49:55 -0300

    Adicionados arquivos Eclipse Github

---
+++ src/git_project/ShowGit.java
@@ -0,0 +1,10 @@
+package git_project;
+
+public class ShowGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Primeiro projeto GITHUB");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Leandro
+ *
+ */
+module teste_Git {
+}
