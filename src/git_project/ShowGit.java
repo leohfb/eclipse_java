cc13df8e7a47f76451c90c067b7d830c257f703d
Leandro <Leandro@DESKTOP-1SAGB56>
2021-01-18 20:54:38 -0300

    Adicionado uma linha

--- src/git_project/ShowGit.java
+++ src/git_project/ShowGit.java
@@ -5,6 +5,7 @@ public class ShowGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Primeiro projeto GITHUB");
+		System.out.println("Teste de save project");
 	}
 
 }
